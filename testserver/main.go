7c1eb3a36031796e84c3505f75be4ad071f4a2dc
unknown <etornham@gmail.com>
2025-01-30 17:03:01 +0100

    CR endpoint done

--- testserver/main.go
+++ testserver/main.go
@@ -2,23 +2,34 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type Task struct {
-	ID      int    `json:"id"`
+	gorm.Model
 	Title   string `json:"title"`
 	Content string `json:"content"`
 }
 
-var tasks = []Task{
-	{ID: 1, Title: "Walk the dog", Content: "you have to take the bags"},
-	{ID: 2, Title: "Wash the dishes", Content: "try not to end it all"},
-	{ID: 3, Title: "Maybe do some situps", Content: "ichi ni san shi"},
+var db *gorm.DB
+
+func init() {
+	var err error
+	db, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	if err := db.AutoMigrate(&Task{}); err != nil {
+		log.Fatal("failed to migrate database:", err)
+	}
 }
 
 func main() {
@@ -26,22 +37,54 @@ func main() {
 	r.HandleFunc("/tasks", getTasksHandler).Methods("GET")
 	r.HandleFunc("/tasks", createTaskHandler).Methods("POST")
 
-	http.ListenAndServe(":5000", handlers.CORS()(r))
+	test := "Server running on :5000"
+	var t *string
+	t = &test
+	fmt.Println(*t)
+
+	corsOptions := handlers.AllowedOrigins([]string{"http://localhost:5173"})
+	corsMethods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"})
+	corsHeaders := handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
+
+	handler := handlers.CORS(corsOptions, corsMethods, corsHeaders)(r)
+
+	http.ListenAndServe(":5000", handler)
 
 }
 
 func getTasksHandler(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(tasks)
+	var tasks []Task
+	result := db.Find(&tasks)
+
+	if result.Error != nil {
+		http.Error(w, "Failed to retrieve tasks", http.StatusInternalServerError)
+		log.Println("Error fetching tasks:", result.Error)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(tasks); err != nil {
+		http.Error(w, "Error encoding tasks", http.StatusInternalServerError)
+		log.Println("Error encoding tasks to JSON:", err)
+		return
+	}
 }
 
 func createTaskHandler(w http.ResponseWriter, r *http.Request) {
 	var newTask Task
+
 	err := json.NewDecoder(r.Body).Decode(&newTask)
 	if err != nil {
-		log.Fatal(err)
-	} else {
-		tasks = append(tasks, newTask)
-		json.NewEncoder(w).Encode(newTask)
+		http.Error(w, "invalid request payload", 400)
+		return
 	}
 
+	if err := db.Create(&newTask).Error; err != nil {
+		http.Error(w, "Couldnt create task", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
 }
